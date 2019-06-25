85f9ee02853238e10e3b18cda35232e82a81ac2a
jutanke <jutanke@googlemail.com>
2019-06-25 17:20:57 +0200

    add geometry function

--- aik/camera.py
+++ aik/camera.py
@@ -1,3 +1,7 @@
+import numpy as np
+import cv2
+
+
 class Camera:
 
     def __init__(self, K, rvec, tvec, dist_coef, w, h):
@@ -9,9 +13,40 @@ class Camera:
         :param w: int
         :param h: int
         """
-        self.K = K
-        self.rvec = rvec
-        self.tvec = tvec
-        self.dist_coef = dist_coef
+        self.K = np.array(K)
+        self.rvec = np.array(rvec)
+        self.tvec = np.array(tvec)
+        self.dist_coef = np.array(dist_coef)
         self.w = w
         self.h = h
+
+        # -- P --
+        R = cv2.Rodrigues(self.rvec)[0]
+        Rt = np.zeros((3, 4))
+        Rt[:, 0:3] = R
+        Rt[:, 3] = self.tvec
+        self.P = self.K @ Rt
+
+        # -- location --
+        self.pos = -np.transpose(R) @ tvec
+
+    
+    def location(self):
+        """ return the camera position in world coordinates
+        """
+        return self.pos
+
+    
+    def project_points(self, points3d):
+        """
+        :param points3d: [n x 3]
+        """
+        if len(points3d.shape) == 1:
+            points3d = np.expand_dims(points3d, axis=0)
+        pts2d, _ = cv2.projectPoints(points3d, self.rvec, self.tvec, self.K, self.dist_coef)
+
+        pts2d = np.squeeze(pts2d)
+        if len(pts2d.shape) == 1:
+            pts2d = np.expand_dims(pts2d, axis=0)
+
+        return pts2d
---
+++ aik/geometry.py
@@ -0,0 +1,68 @@
+import numpy as np
+import cv2
+
+
+def triangulate(point1, point2, cam1, cam2):
+    """ triangulate point1 in cam1 with point2 in cam2
+    :param point1: [2,]
+    :param point2: [2,]
+    """
+    point1 = np.expand_dims(point1, axis=1).astype('float64')  # 2 x n
+    point2 = np.expand_dims(point2, axis=1).astype('float64')
+    P1 = cam1.P
+    P2 = cam2.P
+    point3d = np.squeeze(cv2.triangulatePoints(P1, P2, point1, point2))  # (4, ) // homogenous
+    h = point3d[3]
+    point3d = point3d/h
+    return point3d[0:3]
+
+
+def get_fundamental_matrix(cam1, cam2):
+    """ finds the fundamental matrix between two views
+    :param P1: {3x4} projection matrix
+    :param P2: {3x4} projection matrix
+    :return:
+    """
+    P1 = cam1.P
+    P2 = cam2.P
+
+    points3d = np.array([
+        [0, 0, 0],
+        [1505, 1493, 1501],
+        [300, 300, 0],
+        [1200, 0, 1200],
+        [0, 0, 1355],
+        [1355, 0, 1],
+        [999, 999, 1001],
+        [1005, 1001, 1000],
+        [551, 5, 333],
+        [-100, -100, 1005],
+        [1004, -100, 531],
+        [-999, 5, 33],
+        [-1500,-1000, -503],
+        [99, -99, 99],
+        [-99, 99, 99],
+        [99, 99, -99],
+        [5, 5, 5],
+        [-5, -5, 5],
+        [0.5, 0.5, 0.5],
+        [0.1, 0.9, 0.8],
+        [-0.1, -0.8, -.9]
+    ], 'float64')
+
+    points1 = np.zeros((21, 2))
+    points2 = np.zeros((21, 2))
+    for i, (x, y, z) in enumerate(points3d):
+        p3d = np.array([x, y, z, 1])
+        a1, b1, c1 = P1 @ p3d
+        a2, b2, c2 = P2 @ p3d
+        assert c1 != 0 and c2 != 0
+        points1[i, 0] = a1 / c1
+        points1[i, 1] = b1 / c1
+        points2[i, 0] = a2 / c2
+        points2[i, 1] = b2 / c2
+
+    F, mask = cv2.findFundamentalMat(
+        points1, points2, cv2.FM_8POINT
+    )
+    return F
